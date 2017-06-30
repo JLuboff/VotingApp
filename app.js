68d2d0b4ac25ed6a40dca482fe0c25bc0d9f2449
Jason L <jluboff@gmail.com>
2017-06-30 16:02:29 -0700

    Testing using IP as opposed to session

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express'),
-      hbs = require('hbs'),
-      MongoClient = require('mongodb').MongoClient,
-      ObjectID = require('mongodb').ObjectID,
-      multer = require('multer'),
-      moment = require('moment'),
-      port = process.env.PORT || 3000;
+hbs = require('hbs'),
+MongoClient = require('mongodb').MongoClient,
+ObjectID = require('mongodb').ObjectID,
+multer = require('multer'),
+moment = require('moment'),
+//  session = require('express-session'),
+parseurl = require('parseurl'),
+port = process.env.PORT || 3000;
 
 var app = express();
 let storage = multer.diskStorage({});
 let upload = multer({storage});
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+/*app.use(session({
+secret: 'potato',
+resave: false,
+saveUnitialized: true
+}));
+app.use((req, res, next) => {
 
-MongoClient.connect(`mongodb://localhost:27017/polls`, (err, db)=>{
+let views = req.session.views;
+
+if(!views){
+views = req.session.views = {};
+}
+
+let pathname = parseurl(req).pathname;
+
+views[pathname] = (views[pathname] || 0) + 1;
+
+next();
+
+}); */
+app.use(express.static(__dirname + '/public'));
+MongoClient.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@ds064299.mlab.com:64299/polls`, (err, db)=>{
   if(err) throw err;
 
   app.get('/addpoll', (req, res) => {
@@ -21,36 +42,46 @@ MongoClient.connect(`mongodb://localhost:27017/polls`, (err, db)=>{
   });
 
   app.post('/addpoll', upload.array(), (req, res) => {
-      let poll = {
-        time: moment().format(),
-        pollName: req.body.pollName
-      }
+    let poll = {
+      time: moment().format(),
+      pollName: req.body.pollName,
+      ipAddresses: []
+    }
 
-      for(let i in req.body){
-        if(i !== 'pollName'){
-          poll[i] = {
-            votes: 0,
-            optionName: req.body[i]
-          }
+    for(let i in req.body){
+      if(i !== 'pollName'){
+        poll[i] = {
+          votes: 0,
+          optionName: req.body[i]
         }
-      };
+      }
+    };
 
-      db.collection('poll').insert(poll, (err, doc) => {
-        if(err) throw err;
-res.redirect(`/poll/${doc.ops[0]._id}`);
-      });
+    db.collection('poll').insert(poll, (err, doc) => {
+      if(err) throw err;
+      res.redirect(`/poll/${doc.ops[0]._id}`);
+    });
   });
 
   app.get('/poll/voted/:id/:key', (req, res) => {
+    let url = parseurl(req).pathname;
+    console.log(req.headers);
+
+    /*if(Object.keys(req.session.views).filter(el => el.includes(url.slice(0,37))).length > 1 || req.session.views[url] > 1){
+
+      return res.redirect(`/poll/${req.params.id}`);
+    } */
+
     let option = {};
     option[req.params.key + '.votes'] = 1;
 
-     db.collection('poll').findOneAndUpdate({'_id': ObjectID(req.params.id)}, {$inc: option});
-     res.redirect(`/poll/${req.params.id}`);
+    db.collection('poll').findOneAndUpdate({'_id': ObjectID(req.params.id)}, {$inc: option}, {$push: {ipAddresses: req.headers['x-forwarded-for']}});
+    res.redirect(`/poll/${req.params.id}`);
   })
 
   app.get('/poll/:id', (req, res) => {
-    db.collection('poll').findOne({'_id': ObjectID(req.params.id)}, {'_id': 0, 'time': 0}, (err, doc) => {
+
+    db.collection('poll').findOne({'_id': ObjectID(req.params.id)}, {'_id': 0, 'time': 0, 'ipAddresses': 0}, (err, doc) => {
       if (err) throw err;
 
       let pollName = doc.pollName;
@@ -67,8 +98,6 @@ res.redirect(`/poll/${doc.ops[0]._id}`);
     })
   })
 
-
-
   app.get('/', (req, res) => {
     db.collection('poll').find({}).sort({time: 1}).toArray((err, data) => {
       if(err) throw err;
@@ -77,7 +106,7 @@ res.redirect(`/poll/${doc.ops[0]._id}`);
   });
 
   app.get('/voteResults/:id', (req, res) => {
-    db.collection('poll').findOne({'_id': ObjectID(req.params.id)}, {'_id': 0, 'time': 0, 'pollName': 0}, (err, doc) => {
+    db.collection('poll').findOne({'_id': ObjectID(req.params.id)}, {'_id': 0, 'time': 0, 'pollName': 0, 'ipAddresses': 0}, (err, doc) => {
       if(err) throw err;
 
       res.setHeader('Content-Type', 'application/json');
@@ -88,7 +117,7 @@ res.redirect(`/poll/${doc.ops[0]._id}`);
     })
   })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 });
